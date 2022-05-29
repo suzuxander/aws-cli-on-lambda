9e86371e2deda5a4d97319554514b3584f419a2b
suzuxander <suzuxander@gmail.com>
2022-05-29 22:48:19 +0900

    Add script

---
+++ script/deploy.sh
@@ -0,0 +1,4 @@
+aws cloudformation deploy \
+  --stack-name sample-aws-cli-on-lambda \
+  --template-file template.yml \
+  --capabilities CAPABILITY_NAMED_IAM
---
+++ script/synth.sh
@@ -0,0 +1,4 @@
+cd cdk
+npm i
+npm run build
+npm run synth
